5cf427c444a55819493c87012ed4c05fc4648a16
Austinh1512 <Austinhouchens1510@gmail.com>
2024-07-03 01:44:13 -0400

    complete initial gui

---
+++ .venv/main.py
@@ -0,0 +1,36 @@
+import FreeSimpleGUI as sg
+
+BTN_SIZE = (7, 1)
+
+sg.theme("DarkBlue13")
+
+listbox_column_layout = [
+    [sg.Listbox(values=[], key="-TODOS_LIST-", expand_x=True, expand_y=True)]
+]
+
+buttons_column_layout = [
+    [sg.Button("Complete", key="-COMPLETE_TODO_BTN-", size=BTN_SIZE)],
+    [sg.Button("Edit", key="-EDIT_TODO_BTN-", size=BTN_SIZE)],
+    [sg.Button("Delete", key="-DELETE_TODO_BTN-", size=BTN_SIZE)]
+]
+
+frame_layout = [
+    [sg.Column(listbox_column_layout, expand_x=True, expand_y=True), sg.Column(buttons_column_layout)]
+]
+
+
+layout = [
+    [sg.InputText(key="-ADD_TODO-", tooltip="Add a task"), sg.Button("Add", key="-ADD_TODO_BTN-", size=BTN_SIZE)],
+    [sg.Frame("To-Dos",frame_layout, key="-TODO_FRAME-", expand_x=True)],
+    [sg.Button("Cancel", size=BTN_SIZE)]
+]
+
+window = sg.Window("Todo App", layout)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, "Cancel"): break
+    print(event, values)
+
+window.close()
+
